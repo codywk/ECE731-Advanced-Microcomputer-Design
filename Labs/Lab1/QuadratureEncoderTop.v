b6ae6ff8707c6e6e61ca6a11a65f45826b8000c5
Cody Kaufman <Cody Kaufman>
2016-02-03 14:27:32 -0600

    updated

--- Labs/Lab1/QuadratureEncoderTop.v
+++ Labs/Lab1/QuadratureEncoderTop.v
@@ -1,11 +1,36 @@
-module QuadratureEncoderTop (
+module QuadratureEncoderTop #(N = 38, encCountBits = 26, precBits = 12, stp = 21516)(
 	input clk,
 	input reset,
 	input A,
 	input B,
+	output Trvl, // are we 
+	output FwdBck // forward or backwards motion
 	
 	);
 	
-	reg[N-1:0] fwd, mp, bck; // these registers will save the values for specific points to keep track of total distance
+	reg[N-1:0] Fwd, MP, Bck; // these registers will save the values for specific points to keep track of total distance
+	wire _A, _B; //represent filtered signals
 	
+	//initialize modules used in motion detection sytem
+	EncoderDigitalFilter InputAFilter(.clk(clk), .sig(A), .filteredSig(_A)));
+	EncoderDigitalFilter InputBFilter(.clk(clk), .sig(B), .filteredSig(_B)));
+	
+	//assign internal signals 
+	assign Trvl = MvFwd | MvBck;
+	assign FwdBack = MvFwd;
+	
+	always @ (posedge clk) begin
+	
+		if (reset) begin //check if a reset occurs
+			Fwd = stp;
+			MP = 0;
+			Bck = -stp;
+		end
+		else begin
+			
+		end
+		
+		
+	end
+
 endmodule
